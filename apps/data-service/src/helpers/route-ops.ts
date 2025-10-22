d35f6918b42630a2ec2f5bf5fd9cd242c65f767b
James Cary <eliteeggnog@gmail.com>
2025-10-22 10:51:06 -0500

    add durable objects link click capture

--- apps/data-service/src/helpers/route-ops.ts
+++ apps/data-service/src/helpers/route-ops.ts
@@ -53,3 +53,11 @@ export async function scheduleEvalWorkflow(env: Env, event: LinkClickMessageType
 	const stub = env.EVALUATION_SCHEDULER.get(doId);
 	await stub.collectLinkClick(event.data.accountId, event.data.id, event.data.destination, event.data.country || 'UNKNOWN');
 }
+
+export async function captureLinkClickInBackground(env: Env, event: LinkClickMessageType) {
+	await env.QUEUE.send(event);
+	const doId = env.LINK_CLICK_TRACKER_OBJECT.idFromName(event.data.accountId);
+	const stub = env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	if (!event.data.latitude || !event.data.longitude || !event.data.country) return;
+	await stub.addClick(event.data.latitude, event.data.longitude, event.data.country, moment().valueOf());
+}
--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,9 +1,17 @@
-import { Hono } from 'hono';
+import { captureLinkClickInBackground, getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
-import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 import { LinkClickMessageType } from '@repo/data-ops/zod-schema/queue';
 
+import { Hono } from 'hono';
+
 export const app = new Hono<{ Bindings: Env }>();
+
+app.get('/link-click/:accountId', async (c) => {
+	const accountId = c.req.param('accountId');
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
+});
 
 app.get('/:id', async (c) => {
 	const id = c.req.param('id');
@@ -33,8 +41,19 @@ app.get('/:id', async (c) => {
 			timestamp: new Date().toISOString(),
 		},
 	};
-
-	c.executionCtx.waitUntil(c.env.QUEUE.send(queueMessage));
-
+	c.executionCtx.waitUntil(captureLinkClickInBackground(c.env, queueMessage));
 	return c.redirect(destination);
+});
+
+app.get('/click-socket', async (c) => {
+	const upgradeHeader = c.req.header('Upgrade');
+	if (!upgradeHeader || upgradeHeader !== 'websocket') {
+		return c.text('Expected Upgrade: websocket', 426);
+	}
+
+	const accountId = c.req.header('account-id');
+	if (!accountId) return c.text('No Headers', 404);
+	const doId = c.env.LINK_CLICK_TRACKER_OBJECT.idFromName(accountId);
+	const stub = c.env.LINK_CLICK_TRACKER_OBJECT.get(doId);
+	return await stub.fetch(c.req.raw);
 });
